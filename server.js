c378c8b8b4d31fbf35ab665c57c7ed6c9cc60e65
Sumith B H <85575949+Vector-ops@users.noreply.github.com>
2024-10-02 12:32:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 const startServer = async () => {
-	await connectDB();
+	await connectDB(process.env.MONGODB_URI);
 	app.listen(3000, () => {
 		console.log("Server is running on port 3000");
 	});
